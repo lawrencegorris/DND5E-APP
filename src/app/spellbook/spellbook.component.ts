4bffb98648e9fed5115baa2949a626833904b3f3
lawrencegorris <gorrislawrence.personal@gmail.com>
2021-07-22 15:17:11 +0200

    Able to fetch and show spell data from API in console

--- src/app/spellbook/spellbook.component.ts
+++ src/app/spellbook/spellbook.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-spellbook',
   templateUrl: './spellbook.component.html',
   styleUrls: ['./spellbook.component.scss']
 })
-export class SpellbookComponent implements OnInit {
 
-  constructor() { }
+export class SpellbookComponent{
 
-  ngOnInit(): void {
+ showSpellData(form: NgForm){
+
+  //fetch API data each time form is submitted
+   let userInput = form.value["user-spell-input"];
+    //document.querySelector('#test')!.innerHTML = userInput;
+    this.http.get(`https://www.dnd5eapi.co/api/spells/${userInput}`).subscribe(data=>{
+      console.log('data:', data)
+    });
   }
-
+  constructor(private http: HttpClient){};
 }
+
+
+
